1ec51e699389266d69c499e29915184d642e44dc
Carlos Regis <cr@carlosregis.com>
2023-09-08 02:23:34 +0000

    test: add unit tests for the custom user model

--- ch04-bookstore/accounts/tests.py
+++ ch04-bookstore/accounts/tests.py
@@ -1,1 +1,27 @@
-# Create your tests here.
+# accounts/tests.py
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+User = get_user_model()
+
+
+class CustomUserTests(TestCase):
+    def test_create_user(self):
+        user = User.objects.create_user(
+            username="will", email="will@email.com", password="testpass123"
+        )
+        assert user.username == "will"
+        assert user.email == "will@email.com"
+        assert user.is_active
+        assert not user.is_staff
+        assert not user.is_superuser
+
+    def test_create_superuser(self):
+        admin_user = User.objects.create_superuser(
+            username="superadmin", email="superadmin@email.com", password="testpass123"
+        )
+        assert admin_user.username == "superadmin"
+        assert admin_user.email == "superadmin@email.com"
+        assert admin_user.is_active
+        assert admin_user.is_staff
+        assert admin_user.is_superuser
