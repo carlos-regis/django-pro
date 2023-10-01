5d739dc7fdc27b59aba9df9f37ce42bc210e884d
Carlos Regis <zeca2078@gmail.com>
2023-10-01 03:33:20 +0000

    test: Add docker-compose pytestto circleci config

--- ch04-bookstore/bookstore/settings/environment.py
+++ ch04-bookstore/bookstore/settings/environment.py
@@ -1,4 +1,4 @@
-from pathlib import Path  # noqa: D100
+import os
 
 import environ
 
@@ -11,5 +11,5 @@ env.prefix = "DJANGO_"
 site_root = environ.Path(__file__) - 3  # Root of the project
 
 env_file = site_root(".env")
-if Path.exists(env_file):  # pragma: no cover
+if os.path.exists(env_file):  # pragma: no cover
     environ.Env.read_env(env_file=env_file)
